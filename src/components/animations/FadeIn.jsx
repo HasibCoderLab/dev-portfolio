26696ee51eb9f150d3657728fa7375f984277b63
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-12-17 15:40:49 +0600

    set var

--- src/components/animations/FadeIn.jsx
+++ src/components/animations/FadeIn.jsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 const FadeIn = () => {
+  const [isVasible, setIsVasible] = useState(false);
+ const elementRef =  useRef(null);
+ useEffect(() =>{
+  const observer = new IntersectionObserver(
+    ([entry]) =>{
+      //Trigger animation when enters viewport;
+      if (entry.isIntersecting  && !isVasible) {
+        setIsVasible(true)
+      }
+    }
+  )
+ })
   return (
     <div>FadeIn</div>
   )
