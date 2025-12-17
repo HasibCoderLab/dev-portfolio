f8c19a774728a365ea3b6055db3a6995a2dfd587
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-12-17 15:47:51 +0600

    add logic for animation

--- src/components/animations/FadeIn.jsx
+++ src/components/animations/FadeIn.jsx
@@ -2,23 +2,30 @@ import React, { useEffect, useRef, useState } from 'react'
 
 const FadeIn = () => {
   const [isVasible, setIsVasible] = useState(false);
- const elementRef =  useRef(null);
- useEffect(() =>{
-  const observer = new IntersectionObserver(
-    ([entry]) =>{
-      //Trigger animation when enters viewport;
-      if (entry.isIntersecting  && !isVasible) {
-        setIsVasible(true);
-      }
-    },{
-      threshold:threshold,
-      rootMargin:'0px 0px -50px 0px' // trigger slightly before element
+  const elementRef = useRef(null);
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        //Trigger animation when enters viewport;
+        if (entry.isIntersecting && !isVasible) {
+          setIsVasible(true);
+        }
+      }, {
+      threshold: threshold,
+      rootMargin: '0px 0px -50px 0px' // trigger slightly before element
     }
-  );
-  if (elementRef.current)  {
-    observer.observe(elementRef.current)
-  }
- })
+    );
+    if (elementRef.current) {
+      observer.observe(elementRef.current)
+    }
+    return () => {
+      if (elementRef.current) {
+        observer.unobserve(elementRef.current)
+
+      }
+    }
+  },[threshold,isVasible]
+  )
   return (
     <div>FadeIn</div>
   )
