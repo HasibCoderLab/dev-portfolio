78a23996a61985a6f405c67e6a5431f1401ed7a5
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-12-17 10:45:09 +0600

    styling nav btn

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -53,8 +53,8 @@ const Navbar = () => {
                 key={link.id}
                 onClick={() => handleNavClick(link.id)}
                 className={`text-base font-medium transition-all duration-300 ${activeSection === link.id
-                    ? 'text-white'
-                    : 'text-white/70 hover:text-white'
+                  ? 'text-white'
+                  : 'text-white/70 hover:text-white'
                   }`}
               >
                 {link.label}
@@ -68,19 +68,36 @@ const Navbar = () => {
 
             <button
               onClick={() => handleNavClick('contact')}
-           className="px-7 py-3.5 bg-white text-[#212121] font-medium text-base rounded-[17px] border border-white hover:bg-white/90 transition-all duration-300"
+              className="px-7 py-3.5 bg-white text-[#212121] font-medium text-base rounded-[17px] border border-white hover:bg-white/90 transition-all duration-300"
 
             >Hire Me
             </button>
           </div>
           {/* Mobile Menu */}
           <button
-              onClick={() => setIsMenuOpen(!isMenuOpen)}
-              className=""
-              aria-label="menu"
-            >
-              {isMenuOpen ? <X className='' /> :  <Menu className='' />}
-            </button>
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+            className="md:hidden  text-white hover:text-white/80 transition-colors "
+            aria-label="menu"
+            aria-expanded={isMenuOpen}
+
+          >
+            {isMenuOpen ? <X className='w-6 h-6' /> : <Menu className='w-6 h-6' />}
+          </button>
+        </div>
+      </div>
+      {/* Mobile Menu */}
+      <div
+        className={`md:hidden transition-all duration-300 overflow-hidden ${isMenuOpen ? 'max-h-95 opacity-100' : 'max-h-0 opacity-0'}`}>
+        <div
+          className="">
+          {
+            NAV_LINKS.map((link) => (
+              <button
+                key={link.id}
+                onClick={() => handleNavClick(link.id)}
+                className={`block w-full text-left px-4 py-3 rounded-lg  font-medium transition-all duration-300 ${activeSection===link.id} ? text-white bg-white/70 hover:text-white  hover:bg-white/50`}
+                            ))
+          }
         </div>
       </div>
     </nav>
