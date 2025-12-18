52f0c4b559639aa19dd2fb9a047649e9ada02e94
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-12-18 13:05:26 +0600

    add varients

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Navbar from './components/layout/Navbar'
 import Hero from './components/sections/Hero'
+import About from './components/sections/About'
+
 
 const App = () => {
   return (
@@ -9,6 +11,7 @@ const App = () => {
       <Navbar/>
       <main>
         <Hero/>
+        <About />
       </main>
       </div>
   )
