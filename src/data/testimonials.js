f6b3fe77f84fa93eca4b7ee48992c00867a82ea8
Hasib Hasan <coderhasibh@gmail.com>
2026-02-05 22:40:42 +0600

    includes RUTE's Teachers pic

--- src/data/testimonials.js
+++ src/data/testimonials.js
@@ -2,36 +2,11 @@ import amanPic from "../assets/images/t-image/aman.jpg"
 import shantoPic from "../assets/images/t-image/shanto.jpg"
 import sarminPic from "../assets/images/t-image/sarmin.jpg"
 import somikPic from "../assets/images/t-image/somikPic.jpg"
+import hasanSir from "../assets/images/t-image/hasanSir.jpg"
+import gkmSir from "../assets/images/t-image/gkmSir.jpg"
 export const testimonials = [
   {
     id: 1,
-    name: "Rezwan Aman",
-    role: "CSE Student",
-    institution: "Varendra University",
-    image: amanPic,
-    quote: "Very dedicated and passionate about frontend development.",
-    rating: 5,
-  },
-  {
-    id: 2,
-    name: "Mahfizur Rahman Shanto",
-    role: "Junior Developer",
-    institution: "Metropolitan University Bangladesh",
-    image: shantoPic,
-    quote: "Clean code, great UI sense, and professional mindset.",
-    rating: 5,
-  },
-  {
-    id: 5,
-    name: "Sarmin Khan",
-    role: "Freelancer",
-    institution: "Uttara University",
-    image: sarminPic,
-    quote: "Reliable and creative developer with strong frontend skills.",
-    rating: 4,
-  },
-  {
-    id: 2,
     name: "Md. Imdadul Haque Somik ",
     role: "CSE Student",
     institution: "Rajshahi University of Engineering & Technology (RUET)",
@@ -40,13 +15,50 @@ export const testimonials = [
     rating: 5,
   },
   {
+    id: 2,
+    name: "Rezwan Aman",
+    role: "CSE Student",
+    institution: "Varendra University",
+    image: amanPic,
+    quote: "Very dedicated and passionate about frontend development.",
+    rating: 5,
+  },
+  {
     id: 3,
-    name: "MD Hasan",
+    name: "Mahfizur Rahman Shanto",
+    role: "Junior Developer",
+    institution: "Programming Hero",
+    image: shantoPic,
+    quote: "Clean code, great UI sense, and professional mindset.",
+    rating: 5,
+  },
+  {
+    id: 4,
+    name: "Sarmin Khan",
+    role: "Freelancer",
+    institution: "fiverr",
+    image: sarminPic,
+    quote: "Reliable and creative developer with strong frontend skills.",
+    rating: 4,
+  },
+             
+  {
+    id: 5,
+    name: "Hasan Sarker",
     role: "Assistant Professor of ETE",
     institution: "Rajshahi University of Engineering & Technology (RUET)",
-    image: "/images/projects/project3.png",
+    image: hasanSir,
     quote: "Shows excellent growth and commitment to learning.",
     rating: 5,
   },
+   {
+    id: 6,
+    name: "Dr. G.K.M. Hasanuzzaman",
+    role: " Professor of EEE",
+    institution: "Rajshahi University of Engineering & Technology (RUET)",
+    image: gkmSir,
+    quote: "Shows excellent growth and commitment to learning.",
+    rating: 5,
+  }
   
 ];
