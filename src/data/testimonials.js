c5d85904b049122280a57d4d5ac500dc93af0c07
Hasib Hasan <coderhasibh@gmail.com>
2026-02-05 22:25:53 +0600

    includes photo

--- src/data/testimonials.js
+++ src/data/testimonials.js
@@ -1,12 +1,33 @@
+import amanPic from "../assets/images/t-image/aman.jpg"
+import shantoPic from "../assets/images/t-image/shanto.jpg"
+import sarminPic from "../assets/images/t-image/sarmin.jpg"
 export const testimonials = [
   {
     id: 1,
     name: "Rezwan Aman",
     role: "CSE Student",
     institution: "Varendra University",
-    image: "/images/projects/project3.png",
+    image: amanPic,
     quote: "Very dedicated and passionate about frontend development.",
     rating: 5,
+  },
+  {
+    id: 2,
+    name: "Mahfizur Rahman Shanto",
+    role: "Junior Developer",
+    institution: "Metropolitan University Bangladesh",
+    image: shantoPic,
+    quote: "Clean code, great UI sense, and professional mindset.",
+    rating: 5,
+  },
+  {
+    id: 5,
+    name: "Sarmin Khan",
+    role: "Freelancer",
+    institution: "Uttara University",
+    image: sarminPic,
+    quote: "Reliable and creative developer with strong frontend skills.",
+    rating: 4,
   },
   {
     id: 2,
@@ -26,22 +47,5 @@ export const testimonials = [
     quote: "Shows excellent growth and commitment to learning.",
     rating: 5,
   },
-  {
-    id: 4,
-    name: "Mahfizur Rahman Shanto",
-    role: "Junior Developer",
-    institution: "Metropolitan University Bangladesh",
-    image: "/images/projects/project3.png",
-    quote: "Clean code, great UI sense, and professional mindset.",
-    rating: 5,
-  },
-  {
-    id: 5,
-    name: "Sarmin Khan",
-    role: "Freelancer",
-    institution: "Uttara University",
-    image: "/images/projects/project3.png",
-    quote: "Reliable and creative developer with strong frontend skills.",
-    rating: 4,
-  },
+  
 ];
